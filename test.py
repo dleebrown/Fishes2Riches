843ac0aa4ba8f8a195874370ba1d9d83bc924e45
mreeks91 <reptiledrummer@gmail.com>
2016-12-03 13:52:41 -0500

    Bugfix: Donald eats skunkbugs.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 adjectives=['stinky','smelly','a skunk','a wombat','skerdoodled']
 for i in adjectives:
-    print "you are "+i
+    print "donald is "+i
     print "donald eats "+i+"bugs for breakfast"
     print "hello"
